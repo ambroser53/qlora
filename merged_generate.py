39d65c88e525a6acda6961f0cd7d8fa36c19ec2c
ambrose <ambroser53@gmail.com>
2023-06-12 12:03:26 +0100

    merged generate changes

--- merged_generate.py
+++ merged_generate.py
@@ -1,7 +1,9 @@
 import argparse
+import json
 import sys
 import torch
-from transformers import AutoModelForCausalLM, GenerationConfig, LlamaTokenizer
+from tqdm import tqdm
+from transformers import AutoModelForCausalLM, GenerationConfig, LlamaTokenizer, BitsAndBytesConfig, AutoTokenizer
 from accelerate import infer_auto_device_map
 from peft import PeftConfig, PeftModel
 from utils.prompter import Prompter
@@ -10,11 +12,12 @@ from datasets import load_dataset
 
 def main(args):
     dataset = load_dataset("json", data_files=args.dataset)
+    prompter = Prompter(args.prompt_template)
     temperature = 0.6
     top_p = 0.5
     top_k = 40
     num_beams = 4
-    max_new_tokens = 256
+    max_new_tokens = args.max_new_tokens
 
     generation_config = GenerationConfig(
         temperature=temperature,
@@ -26,40 +29,54 @@ def main(args):
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(device)
+    compute_dtype = (torch.float16 if args.fp16 else (torch.bfloat16 if args.bf16 else torch.float32))
     model = AutoModelForCausalLM.from_pretrained(
         args.model_name_or_path,
         return_dict=True,
         load_in_4bit=args.bits == 4,
         load_in_8bit=args.bits == 8,
-        torch_dtype=torch.float16,
+        torch_dtype=(torch.float32 if args.fp16 else (torch.bfloat16 if args.bf16 else torch.float32)),
         device_map={'': 0},
+        quantization_config=BitsAndBytesConfig(
+            load_in_4bit=args.bits == 4,
+            load_in_8bit=args.bits == 8,
+            llm_int8_threshold=6.0,
+            llm_int8_has_fp16_weight=False,
+            bnb_4bit_compute_dtype=compute_dtype,
+            bnb_4bit_use_double_quant=args.double_quant,
+            bnb_4bit_quant_type=args.quant_type
+        ),
     )
 
-    tokenizer = LlamaTokenizer.from_pretrained(args.model_name_or_path)
+    tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path)
     print("finetune model is_loaded_in_8bit: ", model.is_loaded_in_8bit)
     print(model.hf_device_map)
 
-    if not args.load_8bit:
+    if not args.bits == 4 and not args.bits == 8:
         model.half()
 
     model.eval()
 
-    if torch.__version__ >= "2" and sys.platform != "win32":
+    if torch.__version__ >= "2" and sys.platform != "win32" and args.compile:
         model = torch.compile(model)
 
-    prompter = Prompter(args.prompt_template)
-
-    for data in dataset:
+    for data in tqdm(dataset['train']):
         instruction = data['instruction']
         input = data['input']
         prompt = prompter.generate_prompt(instruction, input)
 
         input_ids = tokenizer.encode(prompt, return_tensors="pt")
-        print(input_ids)
+
         input_ids = input_ids.to(device)
         output_ids = model.generate(input_ids=input_ids, generation_config=generation_config)
 
-        print(tokenizer.decode(output_ids[0], skip_special_tokens=True))
+        with open(args.output_file, "a+") as f:
+            f.write(json.dumps({
+                "instruction": instruction,
+                "input": input,
+                "response": tokenizer.decode(output_ids[0], skip_special_tokens=True),
+                "label": data["output"]
+            }) + '\n')
 
 
 def main_one(args):
@@ -69,7 +86,7 @@ def main_one(args):
     top_p = 0.5
     top_k = 40
     num_beams = 4
-    max_new_tokens = 256
+    max_new_tokens = args.max_new_tokens
 
     generation_config = GenerationConfig(
         temperature=temperature,
@@ -121,7 +138,17 @@ if __name__ == "__main__":
     parser.add_argument("--bits", type=int, default=4)
     parser.add_argument("--lora_weights", type=str, default="tloen/alpaca-lora-7b")
     parser.add_argument("--prompt_template", type=str, default="alpaca")
+    parser.add_argument("--compile", type=bool, default=False)
+    parser.add_argument("--max_new_tokens", type=int, default=64)
+    parser.add_argument("--fp16", type=bool, default=True)
+    parser.add_argument("--bf16", type=bool, default=False)
+    parser.add_argument("--double_quant", type=bool, default=True)
+    parser.add_argument("--quant_type", type=str, default="nf4")  # either fp4 or nf4
+    parser.add_argument("--output_file", type=str, default="eval.jsonl")
     args = parser.parse_args()
+
+    if args.output_file == "eval.jsonl":
+        args.output_file = args.lora_weights + ".jsonl"
 
     if args.dataset is None:
         main_one(args)
