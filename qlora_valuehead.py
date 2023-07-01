d7b2a6300973fdb3fcc3eed8fe581814cadfbbec
ambrose <ambroser53@gmail.com>
2023-07-01 17:44:54 +0100

    targets as ids or labels? tokenise them maybe? targets need eos token? Lets find out!

--- qlora_valuehead.py
+++ qlora_valuehead.py
@@ -461,7 +461,7 @@ class DataCollatorForCausalLM(object):
         assert instances[0] is not None
         # Extract elements
         sources = [f"{self.tokenizer.bos_token}{example['input']}" for example in instances]
-        targets = [f"{example['output'].split()[0]}" for example in instances] ## force to only first token no eos
+        targets = [0 if example['output'].split()[0] == 'Included' else 1 for example in instances] ## force to only first token no eos
         # Tokenize
         tokenized_sources_with_prompt = self.tokenizer(
             sources,
@@ -494,7 +494,7 @@ class DataCollatorForCausalLM(object):
                 input_ids.append(torch.tensor(tokenized_source))
         # Apply padding
         input_ids = pad_sequence(input_ids, batch_first=True, padding_value=self.tokenizer.pad_token_id)
-        # labels = pad_sequence(labels, batch_first=True, padding_value=IGNORE_INDEX) if not self.predict_with_generate else None
+        labels = pad_sequence(targets, batch_first=True, padding_value=IGNORE_INDEX) if not self.predict_with_generate else None
         data_dict = {
             'input_ids': input_ids,
             'attention_mask': input_ids.ne(self.tokenizer.pad_token_id),
