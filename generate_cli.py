9f875256b19207b045484e9a31c6d2c6bfd822fb
ambrose <ambroser53@gmail.com>
2023-07-25 15:02:43 +0100

    cyclelightning comes to save our souls from redemption oh baby lord

--- generate_cli.py
+++ generate_cli.py
@@ -1,20 +1,92 @@
 import argparse
 import json
 import sys
+from dataclasses import dataclass
+from typing import Optional, Any, Union
+
 import torch
 from tqdm import tqdm
-from transformers import AutoModelForCausalLM, GenerationConfig, BitsAndBytesConfig, AutoTokenizer, LlamaTokenizer
+from transformers import AutoModelForCausalLM, GenerationConfig, BitsAndBytesConfig, AutoTokenizer, LlamaTokenizer, \
+    PreTrainedTokenizerBase
 from peft import PeftConfig, PeftModel
+from transformers.utils import PaddingStrategy
+
 from utils.prompter import Prompter
 from datasets import load_dataset
 from torch.utils.data import DataLoader
 from transformers import DataCollatorForSeq2Seq
 import re
+import numpy as np
 
 DEFAULT_BOS_TOKEN = '<s>'
 DEFAULT_EOS_TOKEN = '</s>'
 DEFAULT_UNK_TOKEN = '<unk>'
 DEFAULT_PAD_TOKEN = "[PAD]"
+
+
+@dataclass
+class DataCollatorForCausalLM:
+
+    tokenizer: PreTrainedTokenizerBase
+    model: Optional[Any] = None
+    padding: Union[bool, str, PaddingStrategy] = True
+    max_length: Optional[int] = None
+    pad_to_multiple_of: Optional[int] = None
+    label_pad_token_id: int = -100
+    return_tensors: str = "pt"
+
+    def __call__(self, features, return_tensors=None):
+        if return_tensors is None:
+            return_tensors = self.return_tensors
+        labels = [feature["labels"] for feature in features] if "labels" in features[0].keys() else None
+
+        if labels is not None:
+            max_label_length = max(len(l) for l in labels)
+
+            if self.pad_to_multiple_of is not None:
+                max_label_length = (
+                    (max_label_length + self.pad_to_multiple_of - 1)
+                    // self.pad_to_multiple_of
+                    * self.pad_to_multiple_of
+                )
+
+            padding_side = self.tokenizer.padding_side
+            for feature in features:
+                remainder = [self.label_pad_token_id] * (max_label_length - len(feature["labels"]))
+                if isinstance(feature["labels"], list):
+                    feature["labels"] = (
+                        feature["labels"] + remainder if padding_side == "right" else remainder + feature["labels"]
+                    )
+                elif padding_side == "right":
+                    feature["labels"] = np.concatenate([feature["labels"], remainder]).astype(np.int64)
+                else:
+                    feature["labels"] = np.concatenate([remainder, feature["labels"]]).astype(np.int64)
+
+                remainder = [self.tokenizer.pad_token_id] * (max_label_length - len(feature["input_ids"]))
+                attn_remainder = [0] * (max_label_length - len(feature["attention_mask"]))
+                if isinstance(feature["input_ids"], list):
+                    feature["input_ids"] = (
+                        feature["input_ids"] + remainder if padding_side == "right" else remainder + feature["input_ids"]
+                    )
+                    feature["attention_mask"] = (
+                        feature["attention_mask"] + attn_remainder if padding_side == "right" else attn_remainder + feature["attention_mask"]
+                    )
+                elif padding_side == "right":
+                    feature["input_ids"] = np.concatenate([feature["input_ids"], remainder]).astype(np.int64)
+                    feature['attention_mask'] = np.concatenate([feature['attention_mask'], np.zeros_like(remainder)]).astype(np.int64)
+                else:
+                    feature["input_ids"] = np.concatenate([remainder, feature["input_ids"]]).astype(np.int64)
+                    feature['attention_mask'] = np.concatenate([np.zeros_like(remainder), feature['attention_mask']]).astype(np.int64)
+
+        features = self.tokenizer.pad(
+            features,
+            padding=self.padding,
+            max_length=self.max_length,
+            pad_to_multiple_of=self.pad_to_multiple_of,
+            return_tensors=return_tensors,
+        )
+
+        return features
 
 
 def smart_tokenizer_and_embedding_resize(special_tokens_dict, tokenizer, model):
@@ -50,7 +122,8 @@ def batch_generate(args, dataset, device, generation_config, model, prompter, to
             padding=False),
         remove_columns=original_columns).select(range(args.start_from, len(dataset['train'])))
 
-    collator = DataCollatorForSeq2Seq(tokenizer, return_tensors="pt", padding=True)
+    tokenizer.padding_side = "left"
+    collator = DataCollatorForCausalLM(tokenizer, return_tensors="pt", padding=True)
     batch_iter = DataLoader(dataset['train'], batch_size=args.batch_size, shuffle=False, collate_fn=collator)
 
     for batch in tqdm(batch_iter, total=len(batch_iter)):
